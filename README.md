50604276cdb4a1142391c132dcd62bebd9ffde71
helix-dan <lotus.bat@gmail.com>
2013-10-10 17:24:02 +0800

    init pro

--- README.md
+++ README.md
@@ -2,3 +2,19 @@ bootstrap_in_will_paginate
 ==========================
 
 the style of bootstrap's paginate for will_paginate
+
+
+## Usage
+
+copy the file "bootstrap_link_renderer.rb" to your rails_path/lib
+
+Add this line to your config/application.rb
+
+	config.autoload_paths += Dir["#{config.root}/lib", "#{config.root}/lib/**/"]
+
+and then in your view file
+
+	<%= will_paginate(@users, :renderer => BootstrapLinkRenderer) %>
+
+
+
---
+++ bootstrap_link_renderer.rb
@@ -0,0 +1,28 @@
+class BootstrapLinkRenderer < WillPaginate::ActionView::LinkRenderer
+  protected
+    def page_number(page)
+      unless page == current_page
+        tag(:li, link(page, page, :rel => rel_value(page)))       #设定非当前分页的页码链接
+      else
+        tag(:li, tag(:a, page, :href => "#"), :class => 'active') #设定当前分页的页码链接
+      end
+    end
+    
+    def gap
+      text = @template.will_paginate_translate(:page_gap) { '&hellip;' }
+      %(<li class="disabled"><a href="#">#{text}</a></li>)        #设定gap
+    end
+    
+    def previous_or_next_page(page, text, classname)
+      if page
+        tag(:li, link(text, page, :class => classname))           #设定"上一页"和"下一页"可以使用的的链接
+      else
+        tag(:li, tag(:a, text, :href => "#"), 
+          :class =>'disabled')                                    #设定"上一页"和"下一页"不可用的链接
+      end
+    end
+    
+    def html_container(html)
+      tag(:div, tag(:ul, html), container_attributes)             #加入ul标签
+    end
+end
